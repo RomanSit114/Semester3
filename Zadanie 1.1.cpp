589c20f5c818ecd9de04cfaa16df133db9118b02
RomanSit114 <romansitdik@gmail.com>
2021-09-11 15:56:35 +0300

    Create Zadanie 1.1.cpp

---
+++ Zadanie 1.1.cpp
@@ -0,0 +1,19 @@
+﻿// 2 вариант
+#include <iostream>
+#include <Windows.h>
+#include <cmath>
+using namespace std;
+
+int Num(char a[30]) {
+	int number;
+	number = abs(static_cast<int>(a[0])) % 2 + 1;
+	return number;
+}
+
+int main()
+{
+	cout << "Enter your surname: ";
+	char dlinafamilii[15];//длина фамилии
+	cin >> dlinafamilii;
+	cout << Num(dlinafamilii)<<endl;
+}
